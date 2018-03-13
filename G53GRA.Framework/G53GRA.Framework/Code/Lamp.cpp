fad558483969da2af29fe9c7b6144ccc48fa71e4
spGrainger <sp.grainger@gmail.com>
2018-03-13 17:02:29 +0000

    Refactored lamp

--- G53GRA.Framework/G53GRA.Framework/Code/Lamp.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/Lamp.cpp
@@ -4,106 +4,108 @@
 #define _USE_MATH_DEFINES 
 #include <math.h>
 
-void outerCylinder(float height, float radius_top, float radius_bottom)
+void drawCylinder(float h, float r, int textureNum)
 {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, textureNum);
+
 	//the 'resolution' determines how many quads to draw. num quads = 2pi / resolution
 	float resolution = 0.1f * M_PI; 
-	float x, z;
-	float textureProgress;
-	float ratio = radius_top / radius_bottom;
 
-	glBegin(GL_QUAD_STRIP);
-		for (float t = 0.f; t <= 2 * M_PI; t += resolution)
+	//initial x,z before any rotation
+	float x1 = r;		
+	float z1 = 0.0f;
+	float x2;
+	float z2;
+
+	//angle starts at 0, goes up to 2pi
+	float angle = 0.0f;
+
+	//texture x co-ordinate, this will go from 0 to 1
+	float tx1 = 0.0f;
+	float tx2;
+
+	glBegin(GL_QUADS);
+		do
 		{
-			textureProgress = t / (2 * M_PI);
-			x = radius_bottom*cos(t);
-			z = radius_bottom*sin(t);
+			angle += resolution;
+			tx2 = angle / (2 * M_PI);
+			x2  = r*cos(angle);
+			z2  = r*sin(angle);
 
-			glNormal3f(x, 0, z);
+			//outer face
+			{
+				glNormal3f(x1, 0, z1);
+				glTexCoord2f(tx1, 0);
+				glVertex3f(x1, 0.0f, z1);
 
-			glTexCoord2f(textureProgress, 0);
-			glVertex3f(x, 0.f, z);
-			glTexCoord2f(textureProgress, 1);
-			glVertex3f(x * ratio, height, z * ratio);
-		}
+				glNormal3f(x1, 0, z1);
+				glTexCoord2f(tx1, 1);
+				glVertex3f(x1, h, z1);
 
-		glNormal3f(0, 0, -1);
-		glTexCoord2f(1, 0);
-		glVertex3f(radius_bottom, 0.f, 0.f);
-		glTexCoord2f(1, 1);
-		glVertex3f(radius_top, height, 0.f);
+				glNormal3f(x2, 0, z2);
+				glTexCoord2f(tx2, 1);
+				glVertex3f(x2, h, z2);
+
+				glNormal3f(x2, 0, z2);
+				glTexCoord2f(tx2, 0);
+				glVertex3f(x2, 0.f, z2);
+			}
+
+			//inner face (draw in reverse, invert normals)
+			{
+				glNormal3f(-x2, 0, -z2);
+				glTexCoord2f(tx2, 0);
+				glVertex3f(x2, 0.f, z2);
+
+				glNormal3f(-x2, 0, -z2);
+				glTexCoord2f(tx2, 1);
+				glVertex3f(x2, h, z2);
+
+				glNormal3f(-x1, 0, -z1);
+				glTexCoord2f(tx1, 1);
+				glVertex3f(x1, h, z1);
+
+				glNormal3f(-x1, 0, -z1);
+				glTexCoord2f(tx1, 0);
+				glVertex3f(x1, 0.0f, z1);
+			}
+
+			//update x1 to x2, and so on (for next cycle)
+			x1 = x2;
+			z1 = z2;
+			tx1 = tx2;
+		} while (angle <= 2 * M_PI);
 	glEnd();
-}
-
-void innerCylinder(float h, float tr, float br)
-{
-	float res = 0.1f * M_PI;
-	float x, z;
-	float progress;
-	float ratio = tr / br;
-
-	glBegin(GL_QUAD_STRIP);
-		for (float t = 2 * M_PI; t >= 0; t -= res)
-		{
-			progress = t / (2 * M_PI);
-			x = br*cos(t);
-			z = br*sin(t);
-
-			glNormal3f(-x, 0, -z);
-
-			glTexCoord2f(progress, 0);
-			glVertex3f(x, 0.f, z);
-			glTexCoord2f(progress, 1);
-			glVertex3f(x * ratio, h, z * ratio);
-		}
-		glNormal3f(-1, 0, 0);
-		glTexCoord2f(0, 0);
-		glVertex3f(br, 0.f, 0.f);
-		glTexCoord2f(0, 1);
-		glVertex3f(tr, h, 0.f);
-	glEnd();
-}
-
-void cylinder(float height, float radius_top, float radius_bottom, GLuint textureNum)
-{
-	glEnable(GL_TEXTURE_2D);					//enable textures
-	glBindTexture(GL_TEXTURE_2D, textureNum);	//bind the texture, passed in from the constructor
-
-	//draw a cylinder on the outside and inside, so that it is visible from both inside the lampshade and from the outside
-	outerCylinder(height, radius_top, radius_bottom);
-	innerCylinder(height, radius_top, radius_bottom);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glDisable(GL_TEXTURE_2D);
 }
 
-
 void Lamp::Display() {
 
 	glPushMatrix();
+	{
+		// apply the DisplayableObject attributes (defined in MyScene.cpp)
+		glTranslatef(pos[0], pos[1], pos[2]);		//position
+		glScalef(scale[0], scale[1], scale[2]);		//scale
 
-		glTranslatef(pos[0], pos[1], pos[2]);
-		glScalef(scale[0], scale[1], scale[2]);
-
+		// set the color to white, so that the texure shows in its true colors
 		glColor3f(1, 1, 1);
-		
+
+		// draw the 'light stand' as a cylinder, 20 units tall, 1 unit wide, from 10 units down
 		glTranslatef(0, -10, 0);
-		cylinder(20, 1, 1, textureNum);
+		drawCylinder(20, 1, textureNum);
 
-		glTranslatef(0, 20, 0);
-		//lampshade (textured)
-		cylinder(5, 1, 5, textureNum);
-
-
+		// draw the lampshade also as a cylinder, 5 units tall, 5 units wide, from the top of the light stand
+		glTranslatef(0, 18, 0);
+		drawCylinder(5, 5, textureNum);
+	}
 	glPopMatrix();
 
-	//light (spotlight?)
-	static GLfloat colour[] = { 0.6f, 0.6f, 0.6f };
-	Light* light = new Light(GL_LIGHT3, colour);
-	light->position(pos[0], pos[1] + 12.5, pos[2]);
+	// create a new light instance (on GL_LIGHT3)
+	static GLfloat color[] = { 0.6f, 0.6f, 0.6f };		// a slightly dim white
+	Light* light = new Light(GL_LIGHT3, color);
+	light->position(pos[0], pos[1] + 11, pos[2]);		// position it at the top of the light stand
 	light->Display();
-	//light stand
-	//glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-	//gluCylinder(gluNewQuadric(), 1, 2, 10, 10, 1);
-	//glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
 }
